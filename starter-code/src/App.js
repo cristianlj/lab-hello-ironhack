135e7ed7c7c65f3a0cabad53d92061874dd1f459
Cristian Leandro de Jesus <cristian.jesus@programmers.com.br>
2020-01-09 18:53:05 -0300

    final commit

---
+++ starter-code/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello, world! </h1>
+                <img src="/images/menu-top.svg"></img>
+                <img src="/images/icon1.png"></img>
+                <img src="/images/icon2.png"></img>
+                <img src="/images/icon3.png"></img>
+                <img src="/images/icon4.png"></img>
+                <img src="/images/ironhack-logo.svg"></img>
+                <img src="/images/react-logo.svg"></img>
+            </div>
+        )
+    }
+}
+export default App;
